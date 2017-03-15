b3b8077349944d670a0d632de7fc23fb2779183a
John <johnconley507@hotmail.com>
2017-03-15 00:46:58 -0400

    creatcreating User Manager class

---
+++ JavaStore/src/main/java/com/conley/jsf/musicshop/UserManager.java
@@ -0,0 +1,15 @@
+package com.conley.jsf.musicshop;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class UserManager implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+}
