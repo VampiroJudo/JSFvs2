027db88aa33709269e1d9c010c74f6c01786d172
John <johnconley507@hotmail.com>
2017-03-18 18:53:38 -0400

    creating userDetails.java page and correcting userManager

--- JavaStore/src/main/java/com/conley/jsf/musicshop/UserDetails.java
+++ JavaStore/src/main/java/com/conley/jsf/musicshop/UserDetails.java
@@ -1,15 +1,30 @@
 package com.conley.jsf.musicshop;
 
-import java.io.Serializable;
 import javax.faces.view.ViewScoped;
-import java.inject.Named
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
 
 @Named
 @ViewScoped
 public class UserDetails implements Serializable {
-	
-	private static final long serialVersion = 1L;
-	
-	
 
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private UserManager userManager;
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void onload() {
+        user = userManager.isSignedIn() ? userManager.getCurrentUser() : new User();
+    }
+
+    public String submit() {
+        return userManager.save(user);
+    }
 }
