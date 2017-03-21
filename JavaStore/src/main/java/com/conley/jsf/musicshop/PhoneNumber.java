3064a54f9441c70ff87f50739499aede0a53a869
John <johnconley507@hotmail.com>
2017-03-21 18:34:18 -0400

    adding bean validators to user.java page:

--- JavaStore/src/main/java/com/conley/jsf/musicshop/PhoneNumber.java
+++ JavaStore/src/main/java/com/conley/jsf/musicshop/PhoneNumber.java
@@ -1,5 +1,15 @@
 package com.conley.jsf.musicshop;
 
-public class PhoneNumber {
+import java.io.Serializable;
+
+public class PhoneNumber implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String areaCode;
+	private String officeCode;
+	private String subscriberNumber;
+	
+	
 
 }
--- JavaStore/src/main/java/com/conley/jsf/musicshop/User.java
+++ JavaStore/src/main/java/com/conley/jsf/musicshop/User.java
@@ -1,82 +1,100 @@
 package com.conley.jsf.musicshop;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
 public class User implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private String username;
-	private String password;
-	private String firstName;
-	private String lastName;
-	private String emailAddress;
-	private String phoneNumber;
-	private Date birthDate;
-	private Boolean subscribedToNewsLetter;
-	
-	public String getUserName() {
-		return username;
-	}
-	
-	public void setUserName(String username) {
-		this.username = username;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String getEmailAddress() {
-		return emailAddress;
-	}
-	
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public Date getBirthdate() {
-		return birthDate;
-	}
-	
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-	
-	public Boolean getSubscribedToNewsletter() {
-        return subscribedToNewsLetter;
+    private static final long serialVersionUID = 1L;
+
+    @Pattern(regexp = "[A-Za-z0-9]{2,20}", message = "Please enter a username consisting of only letters and digits, between 2 and 20 characters long.")
+    private String username;
+
+    @Size(min = 8, message = "Your password must consist of at least 8 characters.")
+    private String password;
+
+    @Size(min = 1, max = 30, message = "Please enter a first name between 1 and 30 characters long.")
+    private String firstName;
+
+    @Size(min = 1, max = 30, message = "Please enter a last name between 1 and 30 characters long.")
+    private String lastName;
+
+    @ValidEmailAddress
+    private String emailAddress;
+
+    private PhoneNumber phoneNumber;
+
+    @Past(message = "Your birth date must be in the past.")
+    private Date birthDate;
+
+    @NotNull(message = "Please indicate if you want to subscribe to the newsletter or not.")
+    private Boolean subscribedToNewsletter;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Boolean getSubscribedToNewsletter() {
+        return subscribedToNewsletter;
     }
 
     public void setSubscribedToNewsletter(Boolean subscribedToNewsletter) {
-        this.subscribedToNewsLetter = subscribedToNewsletter;
+        this.subscribedToNewsletter = subscribedToNewsletter;
     }
 }
