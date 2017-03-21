7f704abaa5a4ba51c8bfbc513a9dbbef310fa30e
John <johnconley507@hotmail.com>
2017-03-21 18:25:04 -0400

    finishing PhoneNumberValidator class

--- JavaStore/src/main/java/com/conley/jsf/musicshop/PhoneNumberValidator.java
+++ JavaStore/src/main/java/com/conley/jsf/musicshop/PhoneNumberValidator.java
@@ -3,6 +3,7 @@ package com.conley.jsf.musicshop;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -12,8 +13,41 @@ import javax.faces.validator.ValidatorException;
 
 
 @FacesValidator("com.conley.jsf.PhoneNumber")
-public class PhoneNumberValidator {
-	
-	private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("[0-9-{3}-[0-9]-{3}-[0-9]-{4}")
+public class PhoneNumberValidator implements Validator {
+    private static final Logger LOG = LoggerFactory.getLogger(PhoneNumberValidator.class);
 
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        LOG.debug("validate: [{}]", value);
+
+        PhoneNumber phoneNumber = (PhoneNumber) value;
+        if (phoneNumber != null) {
+            checkAreaCode(phoneNumber.getAreaCode());
+            checkOfficeCode(phoneNumber.getOfficeCode());
+        }
+    }
+
+    private void checkAreaCode(String areaCode) {
+        int firstDigit = Character.digit(areaCode.charAt(0), 10);
+        if (firstDigit == 0 || firstDigit == 1) {
+            throw new ValidatorException(
+                    new FacesMessage("The first digit of the area code of your phone number must not be 0 or 1."));
+        }
+    }
+
+    private void checkOfficeCode(String officeCode) {
+        int firstDigit = Character.digit(officeCode.charAt(0), 10);
+        if (firstDigit == 0 || firstDigit == 1) {
+            throw new ValidatorException(
+                    new FacesMessage("The first digit of the office code of your phone number must not be 0 or 1."));
+        }
+
+        int secondDigit = Character.digit(officeCode.charAt(1), 10);
+        int thirdDigit = Character.digit(officeCode.charAt(2), 10);
+        if (secondDigit == 1 && thirdDigit == 1) {
+            throw new ValidatorException(
+                    new FacesMessage("The second and third digit of the office code of your phone number must not both be 1."));
+        }
+    }
 }
+
