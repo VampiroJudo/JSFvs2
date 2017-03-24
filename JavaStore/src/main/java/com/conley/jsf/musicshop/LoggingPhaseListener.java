59bc12c69d053b9bc12fce3e77a8239039df6bd7
John <johnconley507@hotmail.com>
2017-03-24 13:51:09 -0400

    Adding phase listeners to project

--- JavaStore/src/main/java/com/conley/jsf/musicshop/LoggingPhaseListener.java
+++ JavaStore/src/main/java/com/conley/jsf/musicshop/LoggingPhaseListener.java
@@ -1,5 +1,31 @@
 package com.conley.jsf.musicshop;
 
-public class LoggingPhaseListener {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+
+
+public class LoggingPhaseListener {
+	private static final jdk.internal.instrumentation.Logger LOG = LoggerFactory.getLogger(LoggingPhaseListener.class);
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+	
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		LOG.debug("Before phase: {}", event.getPhaseId());
+	}
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		LOG.debug("After phase: {}", eventgetPhaseId());
+	}
 }
